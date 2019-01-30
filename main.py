721b7bd78de2fe6eef0dd16ca14314d3cdf29156
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-01-30 21:19:14 +0100

    Update
    Update

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 # usr/env/bin Python3.4
 # coding:utf-8
 
+# Import Lib
+import argparse
+
 # Import file
 from Gamemanager import GameManager
 from Maze import Maze
 from Gui import Game_GUI
 
-# Import Lib
-import argparse
-
-# Create a pars for user choice
 def parser_game():
+    """Create a pars for user choice"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--Terminal",  action='store_true' ,help="Game in The terminal")
     parser.add_argument("-g", "--GUI", action='store_true' ,help="Game in The GUI")
     args = parser.parse_args()
     return args
 
-# Create a main for run this applications
 if __name__ == "__main__":
+    """Create a main for run this applications"""
     maze = Maze("maze_1.txt")
     args = parser_game()
     gamemanager = GameManager(maze)
