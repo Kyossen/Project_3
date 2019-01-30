794a9a8d15cee09d8eeaaa6a73063a6930e1885f
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-01-30 22:10:05 +0100

    Update
    Update

--- Item.py
+++ Item.py
@@ -1,12 +1,11 @@
 # usr/env/bin Python3.4
 # coding:utf-8
 
-# Import file
-from Player import Player
 
 class Item:
     def __init__(self, position, symbol):
-        """Init a class for use items with a position and a symbol. self.pickup = False is use for graphic interface"""
+        """Init a class for use items with a position and a symbol.
+        self.pickup = False is use for graphic interface"""
         self.x = position[0]
         self.y = position[1]
         self.symbol = symbol
