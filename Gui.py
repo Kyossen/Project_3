7b5ce4daeb728dada9665e81c7e9e62a00fa215c
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-01-30 22:08:15 +0100

    Update
    Update

--- Gui.py
+++ Gui.py
@@ -5,13 +5,11 @@
 import platform
 
 # Import module
-from pygame.locals import *
 import pygame
 
 # Import file
 from Player import Player
-from Maze import Maze
-from Item import Item
+
 
 class WallSprite(pygame.sprite.Sprite):
     """Create a class for manage the character sprites"""
@@ -19,6 +17,7 @@ class WallSprite(pygame.sprite.Sprite):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
+
 
 class CharactereSprite(pygame.sprite.Sprite):
     """Create a class for manage the player sprite"""
@@ -32,14 +31,15 @@ class CharactereSprite(pygame.sprite.Sprite):
         self.rect.x = new_pos[0] * player_pos
         self.rect.y = new_pos[1] * player_pos
 
+
 class Game_GUI:
 
     def __init__(self, maze, items):
         """Init the game"""
         pygame.init()
-        self.windowWidth = 600
-        self.windowHeight = 480
-        self.screen = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.RESIZABLE)
+        self.Width = 600
+        self.Height = 480
+        self.screen = pygame.display.set_mode((self.Width, self.Height))
         pygame.display.set_caption('The Maze')
         self.maze = maze
         self.player = Player(self.maze.get_player_position())
@@ -52,29 +52,30 @@ class Game_GUI:
 
         # Init the pics for sprites and define user os of user
         if platform.system() == "Windows":
-            self.img_player = pygame.image.load("ressource\MacGyver.png")
-            self.img_background = pygame.image.load("ressource\Background.png")
-            self.img_walls = pygame.image.load("ressource\Wall.png")
-            self.img_npc = pygame.image.load("ressource\Gardien.png")
-            self.img_item_1 = pygame.image.load(r"ressource\baton.png")
-            self.img_item_2 = pygame.image.load("ressource\pipe.png")
-            self.img_item_3 = pygame.image.load("ressource\potion.png")
-            self.img_end_game = pygame.image.load("ressource\youdied.png")
-            self.img_win_game = pygame.image.load("ressource\youwin.png")
+            self.img_player = pygame.image.load(r"ressource\MacGyver.png")
+            self.background = pygame.image.load(r"ressource\Background.png")
+            self.img_walls = pygame.image.load(r"ressource\Wall.png")
+            self.img_npc = pygame.image.load(r"ressource\Gardien.png")
+            self.item_1 = pygame.image.load(r"ressource\baton.png")
+            self.item_2 = pygame.image.load(r"ressource\pipe.png")
+            self.item_3 = pygame.image.load(r"ressource\potion.png")
+            self.img_end_game = pygame.image.load(r"ressource\youdied.png")
+            self.img_win_game = pygame.image.load(r"ressource\youwin.png")
         else:
             self.img_player = pygame.image.load("ressource/MacGyver.png")
-            self.img_background = pygame.image.load("ressource/Background.png")
+            self.background = pygame.image.load("ressource/Background.png")
             self.img_walls = pygame.image.load("ressource/Wall.png")
             self.img_npc = pygame.image.load("ressource/Gardien.png")
-            self.img_item_1 = pygame.image.load(r"ressource/baton.png")
-            self.img_item_2 = pygame.image.load("ressource/pipe.png")
-            self.img_item_3 = pygame.image.load("ressource/potion.png")
+            self.item_1 = pygame.image.load(r"ressource/baton.png")
+            self.item_2 = pygame.image.load("ressource/pipe.png")
+            self.item_3 = pygame.image.load("ressource/potion.png")
             self.img_end_game = pygame.image.load("ressource/youdied.png")
             self.img_win_game = pygame.image.load("ressource/youwin.png")
 
         # Init the sprites
         self.sprite_player = CharactereSprite(self.img_player)
-        self.sprite_items = [WallSprite(self.img_item_1), WallSprite(self.img_item_2), WallSprite(self.img_item_3)]
+        self.sprite_items = [WallSprite(self.item_1), WallSprite(self.item_2),
+                             WallSprite(self.item_3)]
         self.items_list_sprite = pygame.sprite.Group()
         self.character_sprites_list = pygame.sprite.Group()
         self.wall_sprites_list = pygame.sprite.Group()
@@ -129,7 +130,7 @@ class Game_GUI:
 
     def draw_base(self):
         """Print a party the game"""
-        self.screen.blit(self.img_background, (0, 0))
+        self.screen.blit(self.background, (0, 0))
         self.wall_sprites_list.draw(self.screen)
         self.draw_inventory()
         self.items_list_sprite.update()
@@ -147,7 +148,7 @@ class Game_GUI:
         while self.running:
             # For close the game
             for event in pygame.event.get():
-                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     return
                 # Get last position for the player
@@ -167,24 +168,27 @@ class Game_GUI:
             if self.new_position != self.old_position:
 
                 # Check len x and len y for the maze
-                if self.maze.true_or_false_pos(self.new_position) == True:
-                    self.new_symbol = self.maze.get_symbol_at_position(self.new_position)
+                self.new = self.maze.get_symbol_at_position(self.new_position)
+                if self.maze.true_or_false_pos(self.new_position) is True:
+                    self.new_symbol = self.new
                 else:
                     self.new_symbol = None
                     self.rollback()
 
-                # Check the new symbol for walls, if wall is true the player move back position
+                # If wall is true the player move back position
                 if self.new_symbol == "x":
                     self.rollback()
 
                 # Check the new symbol for get items
                 if self.new_symbol == "G" or self.new_symbol == "H" or self.new_symbol == "I":
-                    # Pick up the item and put it in inventory and move the sprite to the right position
+                    # Pick up the item and move the item to the right position
                     for idx, item in enumerate(self.items):
-                        if self.new_position[0] == item.x and self.new_position[1] == item.y and  item.pickup != True:
-                            # The right position is calcul for each round of loop
+                        if self.new_position[0] == item.x and self.new_position[1]\
+                                == item.y and item.pickup is not True:
+                            # The right position is calcu a each round of loop
                             self.sprite_items[idx].rect.x = 565
-                            self.sprite_items[idx].rect.y = 200 + 40 * len(self.player.stock)
+                            self.sprite_items[idx].rect.y = \
+                                200 + 40 * len(self.player.stock)
                             self.items[idx].pickup = True
                             self.player.add_item(self.new_symbol)
 
@@ -200,12 +204,12 @@ class Game_GUI:
             # If self.a = True, so we refresh the screen
             # If self.a = False we keep the pics of the endgame
 
-            if self.a == True:
+            if self.a is True:
                 self.draw_base()
                 self.draw_and_move_player()
             # Refresh the screen and manage the images per second
             pygame.display.flip()
             self.clock.tick(60)
 
-#Close the game
+
 pygame.quit()
