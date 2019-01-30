6648136d3135c1503bbdfafcb12277798d9cc00a
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-01-30 21:18:36 +0100

    Update
    Update

--- Item.py
+++ Item.py
@@ -6,6 +6,7 @@ from Player import Player
 
 class Item:
     def __init__(self, position, symbol):
+        """Init a class for use items with a position and a symbol. self.pickup = False is use for graphic interface"""
         self.x = position[0]
         self.y = position[1]
         self.symbol = symbol
